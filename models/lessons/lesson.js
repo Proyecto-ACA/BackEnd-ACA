cedacd674788725b186462e4589491f5286b8229
luist23 <00127717@uca.edu.sv>
2021-09-13 00:07:31 -0600

    relaciones de leccion agregadas

--- models/lessons/lesson.js
+++ models/lessons/lesson.js
@@ -1,6 +1,9 @@
 const Sequelize = require('sequelize');
 const DataTypes = Sequelize.DataTypes;
 const { sequelize } = require('../../services/initService');
+const Category = require('./category');
+const Level = require('./level');
+const Sign = require('../signs/sign');
 
 const Lesson = sequelize.define('lessons', {
     id: {
@@ -13,7 +16,22 @@ const Lesson = sequelize.define('lessons', {
         type: DataTypes.STRING,
         allowNull: false,
     },
+    level_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    category_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    sign_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
 }, { timestamps: false });
 
+Lesson.belongsTo(Level, { as: 'level',foreignKey: 'level_id' });
+Lesson.belongsTo(Category, { as: 'category',foreignKey: 'category_id' });
+Lesson.belongsTo(Word, { as: 'sign',foreignKey: 'sign_id' });
 
 module.exports = Lesson;
