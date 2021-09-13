a7b165b4f2520243d8f9e6056dfe27b4a4b59cc5
luist23 <00127717@uca.edu.sv>
2021-09-13 00:31:31 -0600

    CRUD category

---
+++ controllers/lessons/category.js
@@ -0,0 +1,82 @@
+const Category = require('../../models/lessons/category');
+
+const transform = (records) => {
+    return records.map((record) => {
+        return {
+            id: record.id,
+            name: record.name,
+        }
+    });
+}
+
+const getAll = (req, res) => {
+    console.log('categorys getAll: ', req.body);
+    try {
+        Category.findAll()
+        .then((result)=>{
+            return res.status(200).send(transform(result));
+        })
+        .catch((e)=>{
+            return res.status(400).send(e);
+        })  ;
+    } catch (err) {
+        console.log('categorys: ', err.message);
+        return res.status(500).send(err);
+    }
+};
+
+const save = (item, res) => {
+    console.log('category save: ', item);
+    try {
+        Category.create(item)
+        .then((result)=>{
+            return res.status(200).json({
+                success: true,
+                data: result,
+            });
+        })
+        .catch((e)=>{
+            return res.status(400).json({
+                success: false,
+                error: e,
+            });
+        });
+    } catch (err) {
+        console.log('category save: ', err.message);
+        return res.status(500).json({
+            success: false,
+            error: err,
+        });
+    }
+};
+
+const update = (item, res) => {
+    console.log('category update: ', item);
+    try {
+        Category.update(item, { where: { id: item.id}})
+        .then((result)=>{
+            return res.status(200).json({
+                success: true,
+                data: result,
+            });
+        })
+        .catch((e)=>{
+            return res.status(400).json({
+                success: false,
+                error: e,
+            });
+        });
+    } catch (err) {
+        console.log('category update: ', err.message);
+        return res.status(500).json({
+            success: false,
+            error: err,
+        });
+    }
+};
+
+module.exports = {
+    getAll: getAll,
+    save: save,
+    update: update,
+};
