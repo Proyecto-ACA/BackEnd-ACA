3f000c510c911a2380e6bcf0d4aecbf66afd698c
luist23 <00127717@uca.edu.sv>
2021-09-12 23:50:58 -0600

    add category lesson

---
+++ models/lessons/category.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+const { sequelize } = require('../../services/initService');
+
+const LessonCategory = sequelize.define('lesson_categorys', {
+    id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: {
+            args: true,
+            msg: { code: 40146, msg: 'Nombre de categoria de leccion ya existe' },
+        }
+    },
+}, { timestamps: false });
+
+
+module.exports = LessonCategory;
