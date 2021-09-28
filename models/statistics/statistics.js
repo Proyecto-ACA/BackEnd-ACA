2693d1f1fa8bd5b2c062f67ccc93b3fc42b396a1
Drubico <dilverpro@gmail.com>
2021-09-27 22:52:28 -0600

    Inicializando

---
+++ models/statistics/statistics.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+const { sequelize } = require('../../services/initService');
+
+const Statistics = sequelize.define('lesson_levels', {
+    id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+    },
+    statistics: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        unique: {
+            args: true,
+            msg: { code: 40146, msg: 'Estadistica ya existe' },
+        }
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: {
+            args: true,
+            msg: { code: 40146, msg: 'Nombre de estadistica ya existe' },
+        }
+    },
+}, { timestamps: false });
+
+
+module.exports = Statistics;
