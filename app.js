09f535aa4afb3226ba8252741f4ba172fa3be5a7
CristianAlfaro <00167917@uca.edu.sv>
2021-10-11 21:09:11 -0600

    merge

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const { initSequelize } = require('./services/initService');
