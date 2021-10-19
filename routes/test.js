f07190a29990b7da595d378cc9037b561bee3644
luist23 <00127717@uca.edu.sv>
2021-10-19 16:20:04 -0600

    add routes test

---
+++ routes/test.js
@@ -0,0 +1,111 @@
+var express = require('express');
+var router = express.Router();
+const Test = require('../controllers/tests/test');
+const Category = require('../controllers/tests/category');
+const Difficulty = require('../controllers/tests/difficulty');
+
+const routes = {
+    categoryGetAll = '/category/getAll',
+    categorySave = '/category/save',
+    categoryDelete = '/category/delete',
+    categoryUpdate = '/category/update',
+
+    difficultyGetAll = '/difficulty/getAll',
+    difficultySave = '/difficulty/save',
+    difficultyDelete = '/difficulty/delete',
+    difficultyUpdate = '/difficulty/update',
+
+    testGetAll = '/test/getAll',
+    testSave = '/test/save',
+    testDelete = '/test/delete',
+    testUpdate = '/test/update',
+}
+
+router.get(routes.categoryGetAll, function(req, res, next) {
+    Category.getAll(req, res);
+});
+
+router.get(routes.categorySave, function(req, res, next) {
+    let item = {
+        id: req.params.id,
+        name: req.params.name,
+    }
+    Category.save(item, res);
+});
+
+router.get(routes.categoryDelete, function(req, res, next) {
+    let item = {
+        id: req.params.id,
+    }
+    Category.delete(item, res);
+});
+
+router.get(routes.categoryUpdate, function(req, res, next) {
+    let item = {
+        id: req.params.id,
+        name: req.params.name,
+    }
+    Category.update(item, res);
+});
+
+router.get(routes.difficultyGetAll, function(req, res, next) {
+    Difficulty.getAll(req, res);
+});
+
+router.get(routes.difficultySave, function(req, res, next) {
+    let item = {
+        id: req.params.id,
+        difficulty: req.params.difficulty, //valor numerico
+        name: req.params.name,
+    }
+    Difficulty.save(item, res);
+});
+
+router.get(routes.difficultyDelete, function(req, res, next) {
+    let item = {
+        id: req.params.id,
+    }
+    Difficulty.delete(item, res);
+});
+
+router.get(routes.difficultyUpdate, function(req, res, next) {
+    let item = {
+        id: req.params.id,
+        difficulty: req.params.difficulty, //valor numerico
+        name: req.params.name,
+    }
+    Difficulty.update(item, res);
+});
+
+router.get(routes.testGetAll, function(req, res, next) {
+    Test.getAll(req, res);
+});
+
+router.get(routes.testSave, function(req, res, next) {
+    let item = {
+        id: req.params.id,
+        name: req.params.name,
+        difficulty: req.params.difficulty, //id
+        category: req.params.category, //id
+    }
+    Test.save(item, res);
+});
+
+router.get(routes.testDelete, function(req, res, next) {
+    let item = {
+        id: req.params.id,
+    }
+    Test.delete(item, res);
+});
+
+router.get(routes.testUpdate, function(req, res, next) {
+    let item = {
+        id: req.params.id,
+        name: req.params.name,
+        difficulty: req.params.difficulty, //id
+        category: req.params.category, //id
+    }
+    Test.update(item, res);
+});
+
+module.exports = router;
