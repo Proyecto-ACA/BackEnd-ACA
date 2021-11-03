408c789666b05fbd3433518245841478b58f15f4
Drubico <00121117@uca.edu.sv>
2021-11-03 14:13:18 -0600

    |03/11|AR==>> Database: Update Categories

--- sql/FillDB.sql
+++ sql/FillDB.sql
@@ -2,19 +2,25 @@ INSERT INTO categorys(name,image)
 VALUES ('Abecedario','https://cdn-icons-png.flaticon.com/512/196/196467.png');
 
 INSERT INTO categorys(name,image)
-VALUES ('Familia','https://cdn-icons-png.flaticon.com/512/547/547612.png');
+VALUES ('Alimentos','https://cdn-icons-png.flaticon.com/512/1147/1147832.png');
+
+INSERT INTO categorys(name,image)
+VALUES ('Calendario','https://cdn-icons-png.flaticon.com/512/425/425868.png');
 
 INSERT INTO categorys(name,image)
 VALUES ('Comunes','https://cdn-icons-png.flaticon.com/512/1588/1588163.png');
 
 INSERT INTO categorys(name,image)
-VALUES ('Alimentos','https://cdn-icons-png.flaticon.com/512/1147/1147832.png');
-
-INSERT INTO categorys(name,image)
-VALUES ('Vestimenta','https://cdn-icons.flaticon.com/png/512/2622/premium/2622809.png?token=exp=1635221497~hmac=8ed4f202cd1d2bbd599a4f07f7f7cf0b');
-
-INSERT INTO categorys(name,image)
 VALUES ('Colores','https://cdn-icons-png.flaticon.com/512/1831/1831908.png');
+
+INSERT INTO categorys(name,image)
+VALUES ('Familia','https://cdn-icons-png.flaticon.com/512/547/547612.png');
+
+INSERT INTO categorys(name,image)
+VALUES ('Numeros','https://cdn-icons-png.flaticon.com/512/1519/1519458.png');
+
+INSERT INTO categorys(name,image)
+VALUES ('Ocupaciones','https://cdn-icons-png.flaticon.com/512/1670/1670457.png');
 
 INSERT INTO categorys(name,image)
 VALUES ('Saludos','https://cdn-icons-png.flaticon.com/512/2278/2278923.png');
@@ -23,14 +29,7 @@ INSERT INTO categorys(name,image)
 VALUES ('Verbos','https://cdn-icons-png.flaticon.com/512/2548/2548513.png');
 
 INSERT INTO categorys(name,image)
-VALUES ('Ocupaciones','https://cdn-icons.flaticon.com/png/512/3281/premium/3281322.png?token=exp=1635221666~hmac=e3b00a37e1a55d05930f4fcbb7ee33e5');
-
-INSERT INTO categorys(name,image)
-VALUES ('Numeros','https://cdn-icons.flaticon.com/png/512/2248/premium/2248043.png?token=exp=1635221687~hmac=5d2af7ab7e237c0eb5e71a3e05828eb4');
-
-INSERT INTO categorys(name,image)
-VALUES ('Calendario','https://cdn-icons.flaticon.com/png/512/955/premium/955080.png?token=exp=1635221715~hmac=546da498e3f89b87992fddadd651e6b2');
-
+VALUES ('Vestimenta','https://cdn-icons-png.flaticon.com/512/2132/2132385.png');
 
 ---------- Abecedario
 
