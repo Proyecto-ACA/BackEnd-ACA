c3eac8a8f48ed424cf619736b2700207c3a49d69
FranHercules <00079517@uca.edu.sv>
2022-06-24 13:39:33 -0600

    Esquema de las funciones para el controlador de test

---
+++ controllers/tests/test.js
@@ -0,0 +1,44 @@
+//Controlador para los test
+
+const getAll = (req, res) => {
+    
+};
+
+const getById = (id, res) => {
+
+};
+
+const getByCategory = (category_id, res) => {
+    
+};
+
+const getByDifficulty = (difficulty_id, res) => {
+    
+};
+
+const getByCategoryAndDifficulty = (difficulty_id, category_id, res) => {
+    
+};
+
+const save = (item, res) => {
+    
+};
+
+const update = (item, res) => {
+    
+};
+
+const deleteItem = (item, res) => {
+    
+};
+
+module.exports = {
+    getAll: getAll,
+    save: save,
+    update: update,
+    deleteItem: deleteItem,
+    getByCategory: getByCategory,
+    getByDifficulty: getByDifficulty,
+    getByCategoryAndDifficulty: getByCategoryAndDifficulty,
+    getById: getById,
+};
