1a825f2bd8eb0e68d2deaa0127cfb4ba085ecb24
IvanLopez19 <00181217@uca.edu.sv>
2022-06-27 23:31:30 -0600

    Archivo transformlevel carpeta lessons controller

---
+++ controllers/lessons/transformLevel.js
@@ -0,0 +1,24 @@
+
+//Este funcion nos permite crear un objeto que funciona como una plantilla de como
+//queremos que esten los datos
+const render = (record) => {
+    return {
+        id: record.id,
+        name: record.name,
+        level: record.level
+    }
+}
+
+//Esta plantilla recorre todos los elementos de un arreglo que se obtiene como
+//resultado de una consulta a la base de datos sobre los niveles
+const transform = (records) => {
+    return records.map((record) => {
+        return render(record);
+    });
+}
+
+//Exportamos las dos funciones anteriores
+module.exports = {
+    transforn: transform,
+    render: render
+}
